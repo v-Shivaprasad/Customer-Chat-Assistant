ce276a243ace65ac15b3f8f7155b31ef527719aa
v-Shivaprasad <velagadaa@gmail.com>
2025-12-27 22:55:15 +0530

    fix: final touch

--- Backend/server.ts
+++ Backend/server.ts
@@ -83,7 +83,7 @@ app.get("/chat/:sessionId", async (req, res) => {
 });
 
 
-app.listen( 3000, "0.0.0.0", () => {
-  console.log(`Server running on port ${process.env.PORT || 3000}`);
+app.listen(3000, "0.0.0.0", () => {
+  console.log(`Server running on port 3000`);
 });
 
