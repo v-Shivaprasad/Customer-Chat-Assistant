afcbca0c07af64ab6c610a91278aca13bbcff789
v-Shivaprasad <velagadaa@gmail.com>
2025-12-27 23:06:12 +0530

    this might work

--- Backend/ChatDb.ts
+++ Backend/ChatDb.ts
@@ -6,9 +6,7 @@ export class CHATDB {
    constructor() {
     this.pool = new Pool({
       connectionString: process.env.DATABASE_URL,
-      ssl: process.env.NODE_ENV === "production"
-        ? { rejectUnauthorized: false }
-        : false,
+      ssl: { rejectUnauthorized: false } 
     });
   }
 
