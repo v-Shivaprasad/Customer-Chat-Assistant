66192f2b23291b1712ad4d7b9b361fbc4ebbba0f
v-Shivaprasad <velagadaa@gmail.com>
2025-12-27 22:48:49 +0530

    fix localhost -> 0.0.0.0

--- Backend/server.ts
+++ Backend/server.ts
@@ -83,6 +83,7 @@ app.get("/chat/:sessionId", async (req, res) => {
 });
 
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen( 3000, "0.0.0.0", () => {
+  console.log(`Server running on port ${process.env.PORT || 3000}`);
 });
+
